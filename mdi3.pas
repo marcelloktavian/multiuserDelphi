56862d1149e1f65415377416665a4ff42e298afe
Marcell Oktavian <35864154+marcelloktavian@users.noreply.github.com>
2021-03-03 19:51:43 +0700

    test

---
+++ mdi3.pas
@@ -0,0 +1,25 @@
+unit mdi3;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls;
+
+type
+  TfrmMdi3 = class(TForm)
+    Panel1: TPanel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMdi3: TfrmMdi3;
+
+implementation
+
+{$R *.dfm}
+
+end.
